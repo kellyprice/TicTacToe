299132e1864cd97cfea21f779085cff2bf84d1be
Kelly Price <kelly.price@whitekite.com>
2022-03-09 22:39:16 +0000

    player label

--- TicTacToe/Instructions.cs
+++ TicTacToe/Instructions.cs
@@ -9,7 +9,7 @@ namespace TicTacToe
             StringBuilder s = new StringBuilder();
 
             s.Append("Player 1 is X\r\n");
-            s.Append("Player 1 is O\r\n");
+            s.Append("Player 2 is O\r\n");
             s.Append("\r\nWhen entering your move, select a number corresponding to the grid below...\r\n");
             s.Append("\r\n1 2 3");
             s.Append("\r\n4 5 6");
